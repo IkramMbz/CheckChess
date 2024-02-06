cec3d67cb4745ed97c85e34d41f4743bb80305aa
unknown <mbechezi.ikram@gmail.com>
2024-02-06 17:34:25 +0100

    final

--- Board.ts
+++ Board.ts
@@ -1,18 +1,19 @@
 export class Board {
+    grid: (any | null)[][];
+
     constructor() {
         this.grid = this.initializeGrid();
     }
 
-    initializeGrid() {
-        // Création d'une grille 8x8 remplie de null pour représenter un plateau vide
-        let grid = [];
+    initializeGrid(): (any | null)[][] {
+        let grid: (any | null)[][] = [];
         for (let i = 0; i < 8; i++) {
             grid.push(new Array(8).fill(null));
         }
         return grid;
     }
 
-    placePiece(piece, position) {
+    placePiece(piece: any, position: { x: number; y: number }): void {
         if (position && this.isPositionValid(position)) {
             this.grid[position.y][position.x] = piece;
         } else {
@@ -20,35 +21,29 @@ export class Board {
         }
     }
     
-
-    movePiece(from, to) {
+    movePiece(from: { x: number; y: number }, to: { x: number; y: number }): void {
         if (this.isPositionValid(from) && this.isPositionValid(to) && !this.isEmpty(from)) {
             const piece = this.grid[from.y][from.x];
-            this.grid[to.y][to.x] = piece; // Déplace la pièce
-            this.grid[from.y][from.x] = null; // Vide l'ancienne position
+            this.grid[to.y][to.x] = piece;
+            this.grid[from.y][from.x] = null;
         }
     }
-    
 
-    getPieceAt(position) {
-        // Retourne la pièce à la position spécifiée
+    getPieceAt(position: { x: number; y: number }): any | null {
         if (this.isPositionValid(position)) {
             return this.grid[position.y][position.x];
         }
         return null;
     }
 
-    isPositionValid(position) {
-        // Vérifie si une position est valide sur le plateau
+    isPositionValid(position: { x: number; y: number }): boolean {
         return position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8;
     }
 
-    isEmpty(position) {
-        // Vérifie si la position spécifiée est valide et si elle est vide
+    isEmpty(position: { x: number; y: number }): boolean {
         if (this.isPositionValid(position)) {
             return this.grid[position.y][position.x] === null;
         }
-        return false; // Retourne false si la position n'est pas valide
+        return false;
     }
-    
 }
