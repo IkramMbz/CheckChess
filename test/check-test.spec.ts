baa87244eee6e4292905d44941ae047f9fe02fdf
unknown <mbechezi.ikram@gmail.com>
2024-02-06 15:46:22 +0100

    t3

--- test/check-test.spec.ts
+++ test/check-test.spec.ts
@@ -23,31 +23,46 @@ const initPiece = (pieceName: string, pieceIsWhite: boolean, pieceCode: string, 
   };
 };
 
+const whitePieces: ChessPiece[] = [
+  initPiece("Roi", true, "K", 1, { letter: "A", number: 1 }),
+  initPiece("Tour", true, "R", 2, { letter: "A", number: 3 }),
+  initPiece("Fou", true, "B", 3, { letter: "A", number: 4 }),
+  initPiece("Cavalier", true, "N", 4, { letter: "A", number: 5 }),
+  initPiece("Pion", true, "P", 5, { letter: "B", number: 1 })
+];
+
+const blackPieces: ChessPiece[] = [
+  initPiece("Roi", false, "K", 1, { letter: "A", number: 1 }),
+  initPiece("Tour", false, "R", 2, { letter: "A", number: 3 }),
+  initPiece("Fou", false, "B", 3, { letter: "A", number: 4 }),
+  initPiece("Cavalier", false, "N", 4, { letter: "A", number: 5 }),
+  initPiece("Pion", false, "P", 5, { letter: "B", number: 1 })
+];
+
 describe('Test moves', () => {
   const e: ChessPiece = initPiece("Roi", true, "K", 1, { letter: "A", number: 1 });
 
-  const whitePieces: ChessPiece[] = [
-    initPiece("Roi", true, "K", 1, { letter: "A", number: 1 }),
-    initPiece("Tour", true, "R", 2, { letter: "A", number: 3 }),
-    initPiece("Fou", true, "B", 3, { letter: "A", number: 4 }),
-    initPiece("Cavalier", true, "N", 4, { letter: "A", number: 5 }),
-    initPiece("Pion", true, "P", 5, { letter: "B", number: 1 })
-  ];
-
-  const blackPieces: ChessPiece[] = [
-    initPiece("Roi", false, "K", 1, { letter: "A", number: 1 }),
-    initPiece("Tour", false, "R", 2, { letter: "A", number: 3 }),
-    initPiece("Fou", false, "B", 3, { letter: "A", number: 4 }),
-    initPiece("Cavalier", false, "N", 4, { letter: "A", number: 5 }),
-    initPiece("Pion", false, "P", 5, { letter: "B", number: 1 })
-  ];
-
   it(`Test moov`, () => {
-    
+    const moviesFound = testMove("A", 6, 1, true);
+    expect(moviesFound).toStrictEqual(true);
   });
 });
 
-const testMove = (x, y, piece) => {
+const pieceExist = (pieceId: number, pieces: ChessPiece[]): boolean => {
+  return pieces.some(piece => piece.pieceId === pieceId);
+};
 
-}
+
+const testMove = (y: string, x: number, pieceId: number, isWhitePlayer: boolean) => {
+  const pieces = isWhitePlayer ? whitePieces : blackPieces;
+
+  if (!pieceExist(pieceId, pieces)) {
+    console.log("La pièce avec l'ID", pieceId, "n'existe pas.");
+    return false;
+  } else {
+    console.log("Les pièces avec l'ID", pieceId, "sont :");
+    pieces.filter(piece => piece.pieceId === pieceId).forEach(piece => console.log(piece));
+    return true;
+  }
+};
 
