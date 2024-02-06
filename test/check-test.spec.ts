beea3bcdb75dc48850f3242a82632ef60d60e5a8
unknown <mbechezi.ikram@gmail.com>
2024-02-06 15:37:43 +0100

    t2

--- test/check-test.spec.ts
+++ test/check-test.spec.ts
@@ -1,7 +1,5 @@
 import { describe, expect, it } from 'vitest'
 
-type Movie = { title: string; year: number; };
-type Movies = Movie[];
 type Position = {
   letter: string;
   number: number;
@@ -28,13 +26,28 @@ const initPiece = (pieceName: string, pieceIsWhite: boolean, pieceCode: string, 
 describe('Test moves', () => {
   const e: ChessPiece = initPiece("Roi", true, "K", 1, { letter: "A", number: 1 });
 
-  const pieces: ChessPiece[] = [
+  const whitePieces: ChessPiece[] = [
     initPiece("Roi", true, "K", 1, { letter: "A", number: 1 }),
-    initPiece("Tour", false, "R", 2, { letter: "A", number: 3 }),
+    initPiece("Tour", true, "R", 2, { letter: "A", number: 3 }),
     initPiece("Fou", true, "B", 3, { letter: "A", number: 4 }),
-    initPiece("Cavalier", false, "N", 4, { letter: "A", number: 5 }),
+    initPiece("Cavalier", true, "N", 4, { letter: "A", number: 5 }),
     initPiece("Pion", true, "P", 5, { letter: "B", number: 1 })
   ];
 
-  console.log(pieces[0])
+  const blackPieces: ChessPiece[] = [
+    initPiece("Roi", false, "K", 1, { letter: "A", number: 1 }),
+    initPiece("Tour", false, "R", 2, { letter: "A", number: 3 }),
+    initPiece("Fou", false, "B", 3, { letter: "A", number: 4 }),
+    initPiece("Cavalier", false, "N", 4, { letter: "A", number: 5 }),
+    initPiece("Pion", false, "P", 5, { letter: "B", number: 1 })
+  ];
+
+  it(`Test moov`, () => {
+    
+  });
 });
+
+const testMove = (x, y, piece) => {
+
+}
+
